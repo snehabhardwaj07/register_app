5def648ebc6bfcd3e3ca59595695ef88990b0550
snehabhardwaj07 <sneha1731.be20@chitkara.edu.in>
2022-10-15 16:04:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './form';
+
+function App() {
+  return (
+    <div>
+      <Form></Form>
+    </div>
+    
+  );
+}
+
+export default App;
